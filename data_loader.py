baa93157ec48a91f11f92f1a497eb1d5ac6f74a6
valentinodde <v.odde43@gmail.com>
2022-12-16 11:27:17 +0100

    add data_loader functions

--- data_loader.py
+++ data_loader.py
@@ -6,3 +6,21 @@ Created on Fri Dec 16 11:17:18 2022
 @author: valentin
 """
 
+
+import json
+
+def load_small_data():
+    with open('data/toy_instance.json') as f:
+        js = json.load(f)
+    return js
+
+def load_medium_data():
+    with open('data/medium_instance.json') as f:
+        js = json.load(f)
+    return js
+
+
+def load_big_data():
+    with open('data/large_instance.json') as f:
+        js = json.load(f)
+    return js
