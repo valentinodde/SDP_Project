90186e656cb40ac85a1d426599e7a3a404f0d08a
= <arthur@lamafactory.com>
2023-01-17 09:37:59 +0100

    Encode staff qualifications

--- create_model.py
+++ create_model.py
@@ -1,5 +1,8 @@
+import string
+
+
 def create_staff_qualifications(data):
     E = [] #Staff qualifications
     for staff_member in data['staff']:
-        E.append(staff_member['qualifications'])
+        E.append([1 if q in staff_member['qualifications'] else 0 for q in list(data['qualifications'])])
     return E
